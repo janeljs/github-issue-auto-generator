8a849a3f8448619be4c39f3f0683976d3cc63b55
janeljs <jisunlim818@gmail.com>
2021-08-03 00:19:33 +0900

    feat: 깃헙 이슈 자동 생성 기능

---
+++ src/main/java/com/jane/github/GitHubController.java
@@ -0,0 +1,56 @@
+package com.jane.github;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class GitHubController {
+    private final String username;
+    private final String repositoryName;
+    private final String token;
+    private final RestTemplate restTemplate;
+    private final HttpHeaders httpHeaders;
+
+    public GitHubController(RestTemplateBuilder builder,
+                            @Value("${github.username}") String username,
+                            @Value("${github.repository.name}") String repositoryName,
+                            @Value("${github.personal.access.token}") String token) {
+        this.username = username;
+        this.repositoryName = repositoryName;
+        this.token = token;
+        this.restTemplate = builder.build();
+        this.httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @GetMapping("/create")
+    public void createIssues(@RequestParam("week_number") int weekNumber,
+                             @RequestParam("start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+                             @RequestParam("issue_count") int issueCount) {
+        String url = "https://api.github.com/repos/" + username + "/" + repositoryName + "/issues";
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, token);
+
+        for (int i = 0; i < issueCount; i++) {
+            String title = startDate.toString();
+            GitHubRequest gitHubRequest = new GitHubRequest(title, List.of("WEEK" + weekNumber), username);
+            HttpEntity<GitHubRequest> request = new HttpEntity<>(gitHubRequest, httpHeaders);
+            restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+            startDate = startDate.plusDays(1);
+            if (i % 7 == 6) {
+                weekNumber++;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/jane/github/GitHubRequest.java
@@ -0,0 +1,16 @@
+package com.jane.github;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GitHubRequest {
+    private String title;
+    private List<String> labels;
+    private String assignee;
+}
